eee2257b1535ee806d93408a79fa4f63df97200e
michaelts1 <41857330+michaelts1@users.noreply.github.com>
2020-08-06 22:24:21 +0300

    browser_action.js

--- src/js/browser_action.js
+++ src/js/browser_action.js
@@ -1,5 +1,14 @@
+"use strict"
+
+/**
+ * @type {object} stores the settings
+ */
 let vars = null
 
+/**
+ * @async
+ * @function Gets the settings from the storage, and updates the displayed settings accordingly
+ */
 async function getVars() {
 	vars = await browser.storage.sync.get()
 
@@ -9,6 +18,10 @@ async function getVars() {
 	$("#auto-harvestron").prop("checked", vars.autoHarvestron)
 }
 
+/**
+ * @async
+ * @function Toggles a setting on/off, and saves the new value to the storage
+ */
 async function toggle(event) {
 	const id = event.target.id
 	const setting = id.replaceAll(/-(.)/g, (match, group1) => match.replace(match, group1.toUpperCase())) // auto-house => autoHouse
