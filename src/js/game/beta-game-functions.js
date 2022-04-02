ff98653b81f003c2770a68d1f7a5e1a8f0d47e57
Michael Tsaban <41857330+michaelts1@users.noreply.github.com>
2022-04-03 00:06:24 +0300

    Fix addon not working without a house

--- src/js/game/beta-game-functions.js
+++ src/js/game/beta-game-functions.js
@@ -133,6 +133,8 @@ const house = {
 	 * @memberof beta-game-functions
 	 */
 	async addCustomBuild() {
+		if ($("#house_level").text() === "None") return
+
 		vars.actionsPending = true
 		// Hide the interface for the duration of this function:
 
@@ -170,6 +172,7 @@ const house = {
 	 * @memberof beta-game-functions
 	 */
 	async selectBuild() {
+		if ($("#house_level").text() === "None") return
 		const itemId = parseInt($("#betabot-select-build").val())
 		await delay(vars.startActionsDelay)
 
