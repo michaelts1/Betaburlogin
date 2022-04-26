7b992315eafc7cd10886f00b61d7b2aa566ea79d
Michael Tsaban <41857330+michaelts1@users.noreply.github.com>
2022-04-26 11:25:40 +0300

    Improve romanize support (up to 3999) and security

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -227,20 +227,26 @@ const helpers = {
 	 * @memberof background
 	 */
 	romanize(num) {
-		if (!num) return ""
+		if (isNaN(parseInt(num))) return ""
 
-		const roman = {
-			L : 50,
-			XL: 40,
-			X : 10,
-			IX: 9,
-			V : 5,
-			IV: 4,
-			I : 1,
-		}
+		const roman = new Map([
+			["M" , 1000],
+			["CM",  900],
+			["D" ,  500],
+			["CD",  400],
+			["C" ,  100],
+			["XC",   90],
+			["L" ,   50],
+			["XL",   40],
+			["X" ,   10],
+			["IX",    9],
+			["V" ,    5],
+			["IV",    4],
+			["I" ,    1],
+		])
 
 		let str = ""
-		for (const [key, value] of Object.entries(roman)) {
+		for (const [key, value] of roman) {
 			const q = Math.floor(num / value)
 			num -= q * value
 			str += key.repeat(q)
